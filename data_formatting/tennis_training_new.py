4cd458c1fab1c7d28858d5ce3989e13fd5f6751d
Dan Warnick <dsw225@lehigh.edu>
2024-06-14 01:05:27 -0400

    Currently working on grabbing data up until match to work on accuracy

--- data_formatting/tennis_training_new.py
+++ data_formatting/tennis_training_new.py
@@ -1,5 +1,6 @@
 import csv
 from collections import Counter
+from datetime import datetime
 
 ## Original file provided by Jeff Sackman Github 2021
 ## https://github.com/JeffSackmann/tennis_atp/blob/master/examples/query_player_season_totals.py
@@ -12,8 +13,8 @@ from collections import Counter
 ## 2015 or SPW% for Sara Errani in 2021.
 
 mw = 'm'            ## 'm' = men, 'w' = women
-yrstart = 2023      ## first season to calculate totals
-yrend = 2023        ## last season to calculate totals
+yrstart = 2022      ## first season to start calculate totals
+yrend = 2023        ## last season to calculate totals, will stop at each date
 match_min = 10      ## minimum number of matches (with matchstats)
                     ## a player must have to be included for a given year
 
@@ -26,10 +27,36 @@ else:
 
 output_path = 'player_season_totals_' + prefix + '_' + str(yrstart) + '_' + str(yrend) + '.csv'
 
+def convert_date(date_str):
+    """
+    Converts a date string in the format YYYYMMDD to a datetime object.
+    """
+    # Convert to datetime object
+    date_obj = datetime.strptime(date_str, "%Y%m%d")
+    # Convert to readable format for now
+    date_obj = date_obj.strftime("%B %d, %Y") 
+    return date_obj
+
+header = ['Player', 'Latest_Up', 'Matches', 'Wins', 'Losses', 'Aces', 'DoubleFs', 'Servept', 'Firstin', 'Firstwon', 'Secondwon',
+          'w_SvGms', 'w_bpSaved', 'w_bpFaced', 'vaces', 'vdfs', 
+               'retpt', 'vfirstin', 'vfirstwon', 'vsecondwon', 'avg_ork']
+player_db =[header]
+
 header = ['Player', 'Year', 'Matches', 'Wins', 'Losses', 'Win%',
           'Ace%', 'DF%', '1stIn', '1st%', '2nd%',
           'SPW%', 'RPW%', 'TPW%', 'DomRatio', 'AVGOrk']
 player_seasons = [header]
+
+def add_stats(row, start):
+    """
+    Combines arrays at start
+    """
+    for k in player_db:
+        if k[0] == row[0]:
+            k[start:] = [sum(x) for x in zip(k[start:], row[start:])]
+            k[1] = row[1]
+            return
+    player_db.append(row)
 
 for yr in range(yrstart, yrend + 1):
     ## load one year of match results
@@ -63,7 +90,9 @@ for yr in range(yrstart, yrend + 1):
         avg_ork = 0
         missing_rk = 0
 
+        
         for k in pmatches:
+            date = convert_date(k[5])
             if k[10] == pl:
                 wins += 1
                 if k[45].isdigit():
@@ -77,41 +106,41 @@ for yr in range(yrstart, yrend + 1):
                     missing_rk += 1
 
         # Remove missing ranks from data
-        avg_ork = (avg_ork) / float(match_count-missing_rk)
+        # avg_ork = (avg_ork) / float(match_count-missing_rk)
         losses = match_count - wins
-        win_perc = wins / float(match_count)   
+        # win_perc = wins / float(match_count)   
 
         ## readable names for serve stats
-        aces, dfs, svpt, firstin, firstwon, secondwon = sum_row[1:7]
+        aces, dfs, svpt, firstin, firstwon, secondwon, w_SvGms, w_bpSaved, w_bpFaced, vaces, vdfs, retpt, vfirstin, vfirstwon, vsecondwon= sum_row[1:16]
         ## calculate common rate stats
-        ace_rate = aces / float(svpt)
-        df_rate = dfs / float(svpt)
-        firstin_rate = firstin / float(svpt)
-        first_win = firstwon / float(firstin)
-        second_win = secondwon / (svpt - float(firstin))
-        spw = firstwon + secondwon
-        spw_rate = spw / float(svpt)
+        # ace_rate = aces / float(svpt)
+        # df_rate = dfs / float(svpt)
+        # firstin_rate = firstin / float(svpt)
+        # first_win = firstwon / float(firstin)
+        # second_win = secondwon / (svpt - float(firstin))
+        # spw = firstwon + secondwon
+        # spw_rate = spw / float(svpt)
 
-        ## raw return stats
-        vaces, vdfs, retpt, vfirstin, vfirstwon, vsecondwon = sum_row[10:16]
-        ## calculate more aggregates
-        rpw = retpt - vfirstwon - vsecondwon
-        rpw_rate = rpw / float(retpt)
+        # ## raw return stats
+        # vaces, vdfs, retpt, vfirstin, vfirstwon, vsecondwon = sum_row[10:16]
+        # ## calculate more aggregates
+        # rpw = retpt - vfirstwon - vsecondwon
+        # rpw_rate = rpw / float(retpt)
 
         ## STATS TO ADD / CONSIDER
         ## SETS % GAMES % HOLD % BRK % TB W %
         ## Serve + Return Rating and add player rank
         ## Maybe add per match stats ie. aces/match
 
-        tpw_rate = (spw + rpw) / (float(svpt) + retpt)
-        dom_ratio = rpw_rate / (1 - spw_rate)
+        # tpw_rate = (spw + rpw) / (float(svpt) + retpt)
+        # dom_ratio = rpw_rate / (1 - spw_rate)
 
-        row = [pl, yr, match_count, wins, losses, win_perc,
-               ace_rate, df_rate, firstin_rate, first_win, second_win,
-               spw_rate, rpw_rate, tpw_rate, dom_ratio, avg_ork]
-        player_seasons.append(row)
+        row = [pl, date, match_count, wins, losses, aces, dfs, svpt, firstin, 
+               firstwon, secondwon, w_SvGms, w_bpSaved, w_bpFaced, vaces, vdfs, 
+               retpt, vfirstin, vfirstwon, vsecondwon, avg_ork]
+        add_stats(row, 2)
 
 with open('csvs/Generated/' + output_path, 'w', newline='') as results:
     writer = csv.writer(results)
-    for row in player_seasons:
+    for row in player_db:
         writer.writerow(row)
