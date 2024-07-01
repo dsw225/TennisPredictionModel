e576d065d42817ff3398507488b806854aa1f83f
Dan Warnick <dsw225@lehigh.edu>
2024-07-01 13:43:05 -0400

    Hopefully fixed frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="full">
       <div className="body">
         <div className="landing">
-          <img src={`${process.env.PUBLIC_URL}/images/BreakPointLogo1.png`} alt="My Image" />
+          <img src={`${process.env.PUBLIC_URL}/images/BreakPointLogo1.png`} alt="Breakpoint.bet logo" />
           <h1>Coming soon</h1>
         </div>
       </div>
