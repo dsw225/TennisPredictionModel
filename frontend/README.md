65a6634b5769e6684e8316adeaae15e20a7bb1e1
Dan Warnick <dsw225@lehigh.edu>
2024-07-01 13:36:10 -0400

    Readding frontend

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+# Hi, I'm Dan, this is my new new portfolio.
+
+I didn't like the old one so this is my newer newer version.
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="full">
+      <div className="body">
+        <div className="landing">
+          <img src={`${process.env.PUBLIC_URL}/images/BreakPointLogo1.png`} alt="My Image" />
+          <h1>Coming soon</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
