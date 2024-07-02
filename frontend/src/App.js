99c9e880226a5aeb0a6e358f68e1d2ce1c2e5322
Dan Warnick <dsw225@lehigh.edu>
2024-07-01 21:58:43 -0400

    Update issues frontend final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="full">
-      <div className="body">
-        <div className="landing">
-          <img src={`${process.env.PUBLIC_URL}/images/BreakPointLogo1.png`} alt="Breakpoint.bet logo" />
-          <h1>Coming soon</h1>
-        </div>
+    <div className="container">
+      <div className="landing">
+        <img src={`${process.env.PUBLIC_URL}/images/BreakPointLogo1.png`} alt="Breakpoint.bet logo" />
+        <h1>Coming soon</h1>
       </div>
     </div>
   );
